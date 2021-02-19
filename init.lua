8cc05956a4a2056526eaf3c0c7a29493dfeef20f
HimbeerserverDE <lpaschek@icloud.com>
2021-02-19 20:27:31 +0100

    Fix unreliable RPC player invisibility (#1)

--- init.lua
+++ init.lua
@@ -8,13 +8,15 @@ dofile(mp .. "/functions.lua")
 
 minetest.register_on_joinplayer(function(player)
 	if player:get_player_name() == "rpc" then
-		local prop = {
-			visual_size = {x = 0, y = 0},
-			collisionbox = {0, 0, 0, 0, 0, 0},
-		}
-		player:set_nametag_attributes({
-			color = {a = 0, r = 255, g = 255, b = 255},
-		})
-		player:set_properties(prop)
+		minetest.after(0, function(player)
+			local prop = {
+				visual_size = {x = 0, y = 0},
+				collisionbox = {0, 0, 0, 0, 0, 0},
+			}
+			player:set_nametag_attributes({
+				color = {a = 0, r = 255, g = 255, b = 255},
+			})
+			player:set_properties(prop)
+		end, player)
 	end
 end)
