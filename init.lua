2ac386aa8d9c2a3bde13ad174d44723e98111abf
HimbeerserverDE <himbeerserverde@gmail.com>
2021-03-08 10:13:47 +0100

    Remove invisibility for RPC player

--- init.lua
+++ init.lua
@@ -5,18 +5,3 @@ local mp = minetest.get_modpath(minetest.get_current_modname())
 dofile(mp .. "/typeconv.lua")
 dofile(mp .. "/rpc.lua")
 dofile(mp .. "/functions.lua")
-
-minetest.register_on_joinplayer(function(player)
-	if player:get_player_name() == "rpc" then
-		minetest.after(0, function(player)
-			local prop = {
-				visual_size = {x = 0, y = 0},
-				collisionbox = {0, 0, 0, 0, 0, 0},
-			}
-			player:set_nametag_attributes({
-				color = {a = 0, r = 255, g = 255, b = 255},
-			})
-			player:set_properties(prop)
-		end, player)
-	end
-end)
